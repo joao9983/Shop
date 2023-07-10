7897853d0129c2bdf8b141d84dad4b196e9e05f5
joao9983 <joaocarlosnepomucenof@gmail.com>
2023-07-10 14:52:50 -0300

    adding listView on homePage [#2]

--- shop/lib/app/widgets/categories_widget.dart
+++ shop/lib/app/widgets/categories_widget.dart
@@ -1,12 +1,20 @@
 import 'package:flutter/material.dart';
 
-class CategoriesWidget extends StatelessWidget {
+import 'dados_test.dart';
+
+class CategoriesWidget extends StatefulWidget {
   const CategoriesWidget({super.key});
 
+  @override
+  State<CategoriesWidget> createState() => _CategoriesWidgetState();
+}
+
+class _CategoriesWidgetState extends State<CategoriesWidget> {
   @override
   Widget build(BuildContext context) {
     final height = MediaQuery.of(context).size.height;
     final width = MediaQuery.of(context).size.width;
+
     return SizedBox(
       width: width * 1,
       height: height * 0.2,
@@ -19,100 +27,41 @@ class CategoriesWidget extends StatelessWidget {
                 color: const Color.fromARGB(255, 252, 236, 189),
                 borderRadius: BorderRadius.circular(width * 0.03)),
             child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
               children: [
                 Padding(
                   padding: EdgeInsets.only(top: height * 0.02),
                   child: Container(
                     width: width,
                     height: height * 0.1,
-                    decoration: const BoxDecoration(
+                    decoration: BoxDecoration(
                       image: DecorationImage(
                         image: NetworkImage(
-                            'https://cdn.pixabay.com/photo/2020/03/30/15/56/carrot-4984620_1280.png'),
+                            categorias.isNotEmpty ? categorias[0].img : ""),
                       ),
                     ),
                   ),
                 ),
+                Container(
+                  width: width * 0.2,
+                  decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.circular(width * 0.05)),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text(categorias.isNotEmpty ? categorias[0].nome : ""),
+                    ],
+                  ),
+                ),
+                SizedBox(
+                  height: height * 0,
+                )
               ],
             ),
           ),
           SizedBox(
             width: width * 0.02,
-          ),
-          Container(
-            width: width * 0.33,
-            decoration: BoxDecoration(
-                color: const Color.fromARGB(255, 252, 236, 189),
-                borderRadius: BorderRadius.circular(width * 0.03)),
-            child: Column(
-              children: [
-                Padding(
-                  padding: EdgeInsets.only(top: height * 0.02),
-                  child: Container(
-                    width: width,
-                    height: height * 0.1,
-                    decoration: const BoxDecoration(
-                      image: DecorationImage(
-                        image: NetworkImage(
-                            'https://cdn.pixabay.com/photo/2020/03/30/15/56/carrot-4984620_1280.png'),
-                      ),
-                    ),
-                  ),
-                ),
-              ],
-            ),
-          ),
-          SizedBox(
-            width: width * 0.02,
-          ),
-          Container(
-            width: width * 0.33,
-            decoration: BoxDecoration(
-                color: const Color.fromARGB(255, 252, 236, 189),
-                borderRadius: BorderRadius.circular(width * 0.03)),
-            child: Column(
-              children: [
-                Padding(
-                  padding: EdgeInsets.only(top: height * 0.02),
-                  child: Container(
-                    width: width,
-                    height: height * 0.1,
-                    decoration: const BoxDecoration(
-                      image: DecorationImage(
-                        image: NetworkImage(
-                            'https://cdn.pixabay.com/photo/2020/03/30/15/56/carrot-4984620_1280.png'),
-                      ),
-                    ),
-                  ),
-                ),
-              ],
-            ),
-          ),
-          SizedBox(
-            width: width * 0.02,
-          ),
-          Container(
-            width: width * 0.33,
-            decoration: BoxDecoration(
-                color: const Color.fromARGB(255, 252, 236, 189),
-                borderRadius: BorderRadius.circular(width * 0.03)),
-            child: Column(
-              children: [
-                Padding(
-                  padding: EdgeInsets.only(top: height * 0.02),
-                  child: Container(
-                    width: width,
-                    height: height * 0.1,
-                    decoration: const BoxDecoration(
-                      image: DecorationImage(
-                        image: NetworkImage(
-                            'https://cdn.pixabay.com/photo/2020/03/30/15/56/carrot-4984620_1280.png'),
-                      ),
-                    ),
-                  ),
-                ),
-              ],
-            ),
           ),
         ],
       ),
